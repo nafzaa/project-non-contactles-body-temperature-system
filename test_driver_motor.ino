39ce041c094a18e1e913b2f46a947d86b13313f0
nafzaa <78883615+nafzaa@users.noreply.github.com>
2021-08-17 20:43:32 +0800

    coding test driver motor

---
+++ test_driver_motor.ino
@@ -0,0 +1,16 @@
+int motor1 = 5;
+int motor2 = 6;
+
+void setup() {
+  pinMode(motor1, OUTPUT);
+  pinMode(motor2, OUTPUT);
+
+  digitalWrite(motor1, HIGH);
+  digitalWrite(motor2, LOW);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
